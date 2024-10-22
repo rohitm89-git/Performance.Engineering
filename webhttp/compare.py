25bf4a8a7a7612437fc675d2c5300619e6c32e6e
rohitm89 <rohitmohanty.cse07@gmail.com>
2024-10-22 18:18:03 +0530

    feat: added write to file

--- webhttp/compare.py
+++ webhttp/compare.py
@@ -8,6 +8,8 @@ with open('./webhttp/target/allArtifacts/reports/TestReport/statistics.json') as
 with open('./webhttp/metrics_sla.json') as sla_file:
     sla_data = json.load(sla_file)
 
+f = open('./resultValidation.txt','a')
+
 # Iterate over each transaction in stats.json
 for transaction, stats in stats_data.items():
     # Get the corresponding SLA values for the transaction
@@ -23,13 +25,20 @@ for transaction, stats in stats_data.items():
         sla_throughput = sla.get('TPS').get('SLA')
 
         print(f"Transaction: {transaction}")
+        f.write(f"Transaction: {transaction}")
         print(f"Error Pct: {error_pct} (SLA: {sla_error_pct})")
+        f.write(f"Error Pct: {error_pct} (SLA: {sla_error_pct})")
         print(f"Throughput: {throughput} (SLA: {sla_throughput})")
+        f.write(f"Throughput: {throughput} (SLA: {sla_throughput})")
 
         if error_pct > sla_error_pct:
             print(f"Error percentage for {transaction} exceeds SLA!")
+            f.write(f"Error percentage for {transaction} exceeds SLA! \n")
 
         if throughput < sla_throughput:
             print(f"Throughput for {transaction} is below SLA!")
+            f.write(f"Throughput for {transaction} is below SLA! \n")
     else:
         print(f"No SLA found for transaction: {transaction}")
+        f.write(f"No SLA found for transaction: {transaction}")
+f.close()
